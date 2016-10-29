59d4d9b45a53bdc375d704ff9442e79c5c6144e6
Alen Dražić <alen.drazic@fer.hr>
2016-10-29 22:08:28 +0200

    zad4 gotov

---
+++ stud/student.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stud
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Jmbag { get; set; }
+        public Gender Gender { get; set; }
+        public Student(string name, string jmbag)
+        {
+            Name = name;
+            Jmbag = jmbag;
+        }
+
+        public override bool Equals(object obj)
+        {
+            // If parameter is null return false.
+            if (obj == null)
+            {
+                return false;
+            }
+
+            // If parameter cannot be cast to Point return false.
+            Student p = obj as Student;
+            if ((System.Object)p == null)
+            {
+                return false;
+            }
+
+            // Return true if the fields match:
+            return (Name == p.Name);
+        }
+
+        public bool Equals(Student p)
+        {
+            // If parameter is null return false:
+            if ((object)p == null)
+            {
+                return false;
+            }
+
+            // Return true if the fields match:
+            return (Name == p.Name);
+        }
+
+        public static bool operator ==(Student s1, Student s2)
+        {
+            return s1.Equals(s2);
+        }
+
+        public static bool operator !=(Student s1, Student s2)
+        {
+            return !s1.Equals(s2);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
+
+    public enum Gender
+    {
+        Male, Female
+    }
+}
